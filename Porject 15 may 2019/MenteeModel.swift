2d427c3aa854d531c32ae1def4efc81c0fa9510e
Marcell Julienne <marcell.julienne>
2019-05-15 11:19:28 +0700

    MenteeModel Added

---
+++ Porject 15 may 2019/MenteeModel.swift
@@ -0,0 +1,18 @@
+//
+//  MenteeModel.swift
+//  Porject 15 may 2019
+//
+//  Created by Marcell Julienne on 15/05/19.
+//  Copyright © 2019 Marcell Julienne. All rights reserved.
+//
+
+import Foundation
+
+class MenteeModel: LearnerModel{
+    var menteeDomain: String
+    
+    init(menteeName: String, menteeAge: Int, menteeHeight: Float, menteeImageProfile: String, menteeDomain: String) {
+        self.menteeDomain = menteeDomain
+        super.init(learnerName: menteeName, learnerAge: menteeAge, learnerHeight: menteeHeight, learnerImageProfile: menteeImageProfile)
+    }
+}
